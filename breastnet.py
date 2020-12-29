1416e557074be715ca5d97d050d5f80918e5c0ae
Goodsea <kutsal_baran@hotmail.com>
2020-12-29 14:52:21 +0300

    Add BreastNet API with tensorflow2 backend

---
+++ breastnet.py
@@ -0,0 +1,160 @@
+import tensorflow.keras.backend as K 
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Activation, Add, BatchNormalization, Concatenate, Conv2D, Dense,    \
+                                    Dropout, GlobalAveragePooling2D, GlobalMaxPooling2D, Input, Lambda, \
+                                    LeakyReLU, MaxPooling2D, Multiply, Permute, Reshape, UpSampling2D   \
+
+def cbam_block(cbam_feature, ratio=8):
+    # Author: @kobiso (https://github.com/kobiso)
+
+    """Contains the implementation of Convolutional Block Attention Module(CBAM) block.
+    As described in https://arxiv.org/abs/1807.06521.
+    """
+
+    cbam_feature = channel_attention(cbam_feature, ratio)
+    cbam_feature = spatial_attention(cbam_feature)
+    return cbam_feature
+
+def channel_attention(input_feature, ratio=8):
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+    channel = input_feature.shape[channel_axis]
+    
+    shared_layer_one = Dense(channel//ratio,
+                             activation='relu',
+                             kernel_initializer='he_normal',
+                             use_bias=True,
+                             bias_initializer='zeros')
+    shared_layer_two = Dense(channel,
+                             kernel_initializer='he_normal',
+                             use_bias=True,
+                             bias_initializer='zeros')
+    
+    avg_pool = GlobalAveragePooling2D()(input_feature)    
+    avg_pool = Reshape((1,1,channel))(avg_pool)
+    assert avg_pool.shape[1:] == (1,1,channel)
+    avg_pool = shared_layer_one(avg_pool)
+    assert avg_pool.shape[1:] == (1,1,channel//ratio)
+    avg_pool = shared_layer_two(avg_pool)
+    assert avg_pool.shape[1:] == (1,1,channel)
+    
+    max_pool = GlobalMaxPooling2D()(input_feature)
+    max_pool = Reshape((1,1,channel))(max_pool)
+    assert max_pool.shape[1:] == (1,1,channel)
+    max_pool = shared_layer_one(max_pool)
+    assert max_pool.shape[1:] == (1,1,channel//ratio)
+    max_pool = shared_layer_two(max_pool)
+    assert max_pool.shape[1:] == (1,1,channel)
+    
+    cbam_feature = Add()([avg_pool,max_pool])
+    cbam_feature = Activation('sigmoid')(cbam_feature)
+
+    if K.image_data_format() == "channels_first":
+        cbam_feature = Permute((3, 1, 2))(cbam_feature)
+    
+    return Multiply()([input_feature, cbam_feature])
+
+def spatial_attention(input_feature):
+    kernel_size = 7
+    
+    if K.image_data_format() == "channels_first":
+        channel = input_feature.shape[1]
+        cbam_feature = Permute((2,3,1))(input_feature)
+    else:
+        channel = input_feature.shape[-1]
+        cbam_feature = input_feature
+    
+    avg_pool = Lambda(lambda x: K.mean(x, axis=3, keepdims=True))(cbam_feature)
+    assert avg_pool.shape[-1] == 1
+    max_pool = Lambda(lambda x: K.max(x, axis=3, keepdims=True))(cbam_feature)
+    assert max_pool.shape[-1] == 1
+    concat = Concatenate(axis=3)([avg_pool, max_pool])
+    assert concat.shape[-1] == 2
+    cbam_feature = Conv2D(filters = 1,
+                    kernel_size=kernel_size,
+                    strides=1,
+                    padding='same',
+                    activation='sigmoid',
+                    kernel_initializer='he_normal',
+                    use_bias=False)(concat)	
+    assert cbam_feature.shape[-1] == 1
+    
+    if K.image_data_format() == "channels_first":
+        cbam_feature = Permute((3, 1, 2))(cbam_feature)
+        
+    return Multiply()([input_feature, cbam_feature])
+
+
+def residual_block(y, nb_channels, _strides=(1, 1), _project_shortcut=False):
+    # Author: @mjdietzx (https://gist.github.com/mjdietzx)
+
+    shortcut = y
+
+    y = Conv2D(nb_channels, kernel_size=(3, 3), strides=_strides, padding='same')(y)
+    y = BatchNormalization()(y)
+    y = LeakyReLU()(y)
+
+    y = Conv2D(nb_channels, kernel_size=(3, 3), strides=(1, 1), padding='same')(y)
+    y = BatchNormalization()(y)
+
+    if _project_shortcut or _strides != (1, 1):
+        shortcut = Conv2D(nb_channels, kernel_size=(1, 1), strides=_strides, padding='same')(shortcut)
+        shortcut = BatchNormalization()(shortcut)
+
+    y = Add()([shortcut, y])
+    y = LeakyReLU()(y)
+
+    return y
+
+
+def BreastNet(input_shape, n_classes):
+    """
+    M. Togaçar, K.B. Özkurt, B. Ergen et al., BreastNet: A novel ˘
+    convolutional neural network model through histopathological images for the diagnosis of breast
+    cancer, Physica A (2019), doi: https://doi.org/10.1016/j.physa.2019.123592 .
+    """
+
+    dropRate = 0.3
+    
+    init = Input(input_shape)
+    x = Conv2D(32, (3, 3), activation=None, padding='same')(init) 
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(32, (3, 3), activation=None, padding='same')(x) 
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x1 = MaxPooling2D((2,2))(x)
+    
+    x = Conv2D(64, (3, 3), activation=None, padding='same')(x1)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = cbam_block(x)
+    x = residual_block(x, 64)
+    x2 = MaxPooling2D((2,2))(x)
+    
+    x = Conv2D(128, (3, 3), activation=None, padding='same')(x2)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = cbam_block(x)
+    x = residual_block(x, 128)
+    x3 = MaxPooling2D((2,2))(x)
+    
+    ginp1 = UpSampling2D(size=(2, 2), interpolation='bilinear')(x1)
+    ginp2 = UpSampling2D(size=(4, 4), interpolation='bilinear')(x2)
+    ginp3 = UpSampling2D(size=(8, 8), interpolation='bilinear')(x3)
+    
+    hypercolumn = Concatenate()([ginp1, ginp2, ginp3]) 
+    gap = GlobalAveragePooling2D()(hypercolumn)
+
+    x = Dense(256, activation=None)(gap)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Dropout(dropRate)(x)
+    
+    x = Dense(256, activation=None)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    y = Dense(n_classes, activation="softmax", name="BreastNet")(x)
+   
+    model = Model(init, y)
+    return model
